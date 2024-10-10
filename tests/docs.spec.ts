fa3662f98237ccf91a9ba2b64ae15d14f77d2a9c
johnstewart00 <stew.john560@gmail.com>
2024-10-10 14:55:42 -0600

    small change to docs test

--- tests/docs.spec.ts
+++ tests/docs.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from "playwright-test-coverage";
 test("test", async ({ page }) => {
   await page.goto("/docs");
   const header = page.getByRole("heading", { name: "JWT Pizza API" });
-  await expect(page.getByText("[POST] /api/auth")).toBeVisible();
+  await expect(header).toBeVisible();
+  await expect(page.getByText("Register a new user")).toBeVisible();
 });

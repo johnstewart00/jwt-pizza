fbd68a2a96848c22cdbec503df84ba15249a7632
johnstewart00 <stew.john560@gmail.com>
2024-10-10 15:04:21 -0600

    add edit back

--- tests/docs.spec.ts
+++ tests/docs.spec.ts
@@ -4,4 +4,6 @@ test("test", async ({ page }) => {
   await page.goto("/docs");
   const header = page.getByRole("heading", { name: "JWT Pizza API" });
   await expect(header).toBeVisible();
+  const register = page.getByText("Register a new user");
+  await expect(register).toBeVisible();
 });

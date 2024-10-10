ee387078b22ca766d472fd5f0113a8c00bbcb7e3
johnstewart00 <stew.john560@gmail.com>
2024-10-10 15:36:43 -0600

    mock /docs from the backend

--- tests/docs.spec.ts
+++ tests/docs.spec.ts
@@ -1,6 +1,33 @@
 import { test, expect } from "playwright-test-coverage";
 
 test("test", async ({ page }) => {
+  await page.route("*/**/api/docs", async (route) => {
+    const res = {
+      version: 1,
+      endpoints: [
+        {
+          method: "POST",
+          path: "/api/auth",
+          description: "Register a new user",
+          response: {
+            user: {
+              id: 2,
+              name: "pizza diner",
+              email: "d@jwt.com",
+              roles: [
+                {
+                  role: "diner",
+                },
+              ],
+            },
+            token: "tttttt",
+          },
+        },
+      ],
+      config: {},
+    };
+    await route.fulfill({ json: res });
+  });
   await page.goto("/docs");
   const header = page.getByRole("heading", { name: "JWT Pizza API" });
   await expect(header).toBeVisible();

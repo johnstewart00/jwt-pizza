400efad596f6c1c4f1fd520544213bd5a8413ffb
johnstewart00 <stew.john560@gmail.com>
2024-10-10 14:57:30 -0600

    Small edit

--- tests/docs.spec.ts
+++ tests/docs.spec.ts
@@ -4,5 +4,4 @@ test("test", async ({ page }) => {
   await page.goto("/docs");
   const header = page.getByRole("heading", { name: "JWT Pizza API" });
   await expect(header).toBeVisible();
-  await expect(page.getByText("Register a new user")).toBeVisible();
 });
